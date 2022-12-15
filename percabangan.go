96060963d62b5de1f00eebfc132075c930cfc2a2
Wawan <wawanqurniawan19@gmail.com>
2022-12-15 11:05:53 +0700

    Belajar percabangan dan perulangan

--- percabangan.go
+++ percabangan.go
@@ -4,4 +4,11 @@ import "fmt"
 
 func main() {
 	fmt.Println("Materi Perulangan")
+	var nilai = 90
+
+	if nilai >= 80 {
+		fmt.Println("Kamu Lulus, Karena nilai adalah = ", nilai)
+	} else {
+		fmt.Println("Kamu Tidak Lulus, arena nilai adalah = ", nilai)
+	}
 }
--- perulangan.go
+++ perulangan.go
@@ -4,4 +4,7 @@ import "fmt"
 
 func main() {
 	fmt.Println("Materi Perulangan Golang")
+	for i := 0; i < 5; i++ {
+		fmt.Println("Tampilan ke -", i+1)
+	}
 }
