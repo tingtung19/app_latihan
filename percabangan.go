9888ed0f5bbe187900854b925fa9313f7edfacf8
Wawan <wawanqurniawan19@gmail.com>
2022-12-15 11:08:48 +0700

    Coba perulangan

--- percabangan.go
+++ percabangan.go
@@ -8,6 +8,9 @@ func main() {
 
 	if nilai >= 80 {
 		fmt.Println("Kamu Lulus, Karena nilai adalah = ", nilai)
+	} else if nilai >= 70 {
+		fmt.Println("Kamu Harus Matrikulasi, arena nilai adalah = ", nilai)
+		fmt.Println("Kamu pasti bisa,terus berjuang")
 	} else {
 		fmt.Println("Kamu Tidak Lulus, arena nilai adalah = ", nilai)
 	}
