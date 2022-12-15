6b1e7e58d425ccc40a70b2c0f51230eb8c7fb6cd
Wawan <wawanqurniawan19@gmail.com>
2022-12-15 10:31:54 +0700

    Membuat array

--- hello.go
+++ hello.go
@@ -5,4 +5,11 @@ import "fmt"
 func main() {
 	var nama = "Wawan Qurniawan"
 	fmt.Println("Halo ", nama, " selamat belajar golang semangat backend")
+
+	var nilai = [5]int{11, 22, 33, 44, 55}
+
+	for i := 0; i < len(nilai); i++ {
+		fmt.Println(nilai[i])
+	}
+
 }
